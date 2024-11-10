e428fa849a69c9ddd9ef4906ae1aa9cafc5f0f9b
jayanth-2000 <jayanthth2002@gmail.com>
2024-11-10 22:11:15 +0100

    adding remaining files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='dsss_homework02',
+   version='1.0',
+   description='Homework for DSSS exercise 02',
+   author='Jayanth',
+   author_email='jayanthth2002@gmail.com',
+   packages=['math_quiz'], 
+   install_requires=['numpy', 'pandas','sklearn'], 
+)
